4ae97f985d1a70fd5d09cfdf290ba833da0bf455
gndhstr <ghandhisatria@gmail.com>
2023-03-29 09:38:02 +0700

    Modfikasi index.php hero section

--- index.php
+++ index.php
@@ -85,6 +85,38 @@ if (!isset($_SESSION['jabatan'])) {
     </div>
   </header><!-- End Header -->
 
+  <section id="home" class="hero">
+
+    <div class="info d-flex align-items-center">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-lg-6 text-center">
+            <h2 data-aos="fade-down">Hello, World! <br> <span>I'm Gandhi Satria</span></h2>
+            <a data-aos="fade-up" data-aos-delay="200" href="#about" class="btn-get-started">Know More</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div id="hero-carousel" class="carousel slide" data-bs-ride="carousel" data-bs-interval="5000">
+
+      <div class="carousel-item active" style="background-image: url(https://cdn.discordapp.com/attachments/875383813411311627/998198053938921502/Makima.Chainsaw.Man.full.3365772.jpg)"></div>
+      <div class="carousel-item" style="background-image: url(https://cdn.discordapp.com/attachments/875383813411311627/1043564512642613409/sarah-lee-QURU8IY-RaI-unsplash-picsayc.jpg)"></div>
+      <div class="carousel-item" style="background-image: url(https://cdn.discordapp.com/attachments/875383813411311627/1043204616285864036/image.png)"></div>
+      <div class="carousel-item" style="background-image: url(https://cdn.discordapp.com/attachments/875383813411311627/1034763380621324308/unknown.png)"></div>
+      <div class="carousel-item" style="background-image: url(https://cdn.discordapp.com/attachments/875383813411311627/1038010648669470820/c72895d0671d049724ccefc81ec5f0c7.jpg)"></div>
+
+      <a class="carousel-control-prev" href="#hero-carousel" role="button" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon bi bi-chevron-left" aria-hidden="true"></span>
+      </a>
+
+      <a class="carousel-control-next" href="#hero-carousel" role="button" data-bs-slide="next">
+        <span class="carousel-control-next-icon bi bi-chevron-right" aria-hidden="true"></span>
+      </a>
+
+    </div>
+
+  </section>
   <!-- ======= Hero Section ======= -->
   <section id="hero">
     <div class="hero-container">
